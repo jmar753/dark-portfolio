a935cf138522600ab70b2144ab288d280363a96f
Jr Mohan <jmar7533@gmail.com>
2024-03-25 15:42:37 -0400

    colour schema to check

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,10 +3,10 @@ export default function Introduction(){
         <div className="min-h-screen w-full flex items-center justify-center text-white">
             <div className="max-w-6xl">
                 <div className="p-20 flex flex-col space-y-4">
-                    <h1 className="font-bold text-8xl">Hi, I'm JR Mohan!</h1>
-                    <h1 className="font-semibold text-4xl ">A Full-Stack Developer</h1>
+                    <h1 className="font-bold text-8xl">Hi, I'm JR Mohan<span className="text-orange-500">!</span></h1>
+                    <h1 className="font-semibold text-4xl ">A <span className="text-orange-500">Full-Stack Developer</span></h1>
                     <h1 className="text-xl">I'm a new grad who spent the last year building applications for a pretty cool startup, and making websites for some great people. I'm also a powerlifter (incase you've got some paperweights you need to move around🏋️). Let's connect!</h1>
-                    <button className="w-36 h-12 text-xl rounded-md bg-orange-400 text-black">Contact Me</button>
+                    <button className="w-36 h-12 text-xl rounded-md bg-orange-500 text-neutral-900 hover:bg-orange-600 transition duration-100">Contact Me</button>
                 </div>
             </div>
         </div>
