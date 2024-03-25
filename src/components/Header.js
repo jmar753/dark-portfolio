4eec6bd5af8bc837c22a9f730fb67070c7d17eb3
Jr Mohan <jmar7533@gmail.com>
2024-03-25 15:06:47 -0400

    bones done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { IconContext } from "react-icons";
+
 export default function Header(){
     return(
-        <div className="bg-green-400 text-white w-full flex justify-between font-semibold h-11 fixed top-0 left-14 z-40 text-xl ">
-            Hello, this is a test
+        <div className="bg-green-400 text-white w-[calc(100vw-3.5rem)] flex justify-between items-center font-semibold h-16 fixed top-0 left-14 z-40 text-xl px-10">
+            <IconContext.Provider value={{ className: 'text-red-500 text-xl size-6' }}>
+                <div className="flex gap-x-4">
+                    <FaGithub/>
+                    <FaLinkedin/>
+                    <IoMdMail/>
+                </div>
+            </IconContext.Provider>
+            
+            <div>
+                <button className="bg-yellow-400">My Resume</button>
+            </div>
         </div>
     )
 }
--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,7 +1,10 @@
 export default function SideNav(){
     return(
-        <div class="h-screen bg-red-400 sticky top-0 w-14 z-50">
-        
+        <div class="h-screen bg-red-400 sticky top-0 w-14 z-50 flex-col">
+            <div className="h-16 w-14">Jr</div>
+            <div className="w-14 h-32 flex justify-center items-center">
+                <h1 className="rotate-90">something</h1>
+            </div>
         </div>
     )
 }
