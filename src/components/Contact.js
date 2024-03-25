051e8dface1bcbe14410e226481875d8222f7c45
Jr Mohan <jmar7533@gmail.com>
2024-03-25 12:21:27 -0400

    barebones done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,13 @@
 export default function Contact(){
     return(
         <div className="min-h-screen w-full flex items-center justify-center text-white">
-            Hello, this is a test
+            <div className="max-w-6xl text-center">
+                <div className="p-20 flex flex-col space-y-8">
+                    <h1 className="font-bold text-8xl">Contact.</h1>
+                    <h1 className="text-xl">Send an email to connect with me! You can find me on LinkedIn if you want to shoot me a message</h1>
+                    <h1 className="text-xl">jr.mohan753@gmail.com</h1>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -18,7 +18,20 @@ export default function Timeline(){
                         <span className="flex-shrink font-bold text-6xl pr-10">Timeline.</span>
                         <div class="flex-grow border-t border-gray-400"></div>
                     </div>
-                    <div className="flex space-x-6">
+                    <div className="space-y-6">
+                        <div className="space-y-4">
+                            <div className="flex justify-between"><p>lmao</p><p>lmao</p></div>
+                            <div className="flex justify-between"><p>lmao</p><p>lmao</p></div>
+                            <p className="text-xl ">Right now, I'm learning all the things I wanted to learn before, like  Python, Unity, Java (I'm gonna be posting all my silly lil' projects :D) and even things other things like tying fishing lines, making D&D characters and Indonesian.</p>
+                            <div className="flex items-start flex-wrap gap-2 w-full">
+                                {funTag.map((item, index) => (
+                                    <div className="bg-orange-400 rounded-2xl px-2 py-0.5" key={index}>
+                                        {item.value}
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+
                         <div className="space-y-4">
                             <div className="flex justify-between"><p>lmao</p><p>lmao</p></div>
                             <div className="flex justify-between"><p>lmao</p><p>lmao</p></div>
